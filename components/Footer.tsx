8fc7e0e5409b6185bdead5b442179c0d4521b664
Aashish Neupane <162073333+Aashis719@users.noreply.github.com>
2025-10-22 22:12:15 +0545

    changes in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Dumbbell, Twitter, Instagram, Facebook } from 'lucide-react';
 import { NAV_LINKS } from '../constants';
-
+ 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-dark-2 border-t border-gray-800">
