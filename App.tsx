f417852473c8327b807fc474830acc89953b89d7
Aashish Neupane <162073333+Aashis719@users.noreply.github.com>
2025-11-27 20:49:33 +0545

    Fix changes

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); 
   }, [pathname]);
 
   return null;
